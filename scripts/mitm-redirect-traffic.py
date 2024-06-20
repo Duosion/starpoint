b3055bf0df46fd5908046796e6c857d991ae2afe
Duosion <duosiont@gmail.com>
2024-06-19 22:55:31 -0500

    Adjust the mitm-redirect-traffic script to query IP addresses instead of hardcoding IPs.

--- scripts/mitm-redirect-traffic.py
+++ scripts/mitm-redirect-traffic.py
@@ -6,32 +6,29 @@ API_SCHEME = 'http'
 
 prefixes = ["/openapi", "/infodesk", ""]
 
+import socket
+
 # hostname: prefix_index
-hosts = {
+hostnames = {
     # openapi
     "openapi-zinny3.game.kakao.com": 0,
     "gc-openapi-zinny3.kakaogames.com": 0,
-    "3.35.189.170": 0,
-    "15.164.132.131": 0,
-    "18.160.60.78": 0,
-    "18.160.60.56": 0,
-    "18.160.60.41": 0,
-    "18.160.60.124": 0,
 
     # infodesk
     "gc-infodesk-zinny3.kakaogames.com": 1,
-    "18.64.155.6": 1,
-    "18.64.155.47": 1,
-    "18.64.155.84": 1,
-    "18.64.155.74": 1,
 
     # na server
     "na.wdfp.kakaogames.com": 2,
-    "3.210.14.73": 2,
-    "3.222.140.107": 2,
-    "52.44.29.229": 2,
-    "3.229.21.155": 2
 }
+
+# Resolve hostnames to IP addresses
+hosts = {}
+for hostname, prefix_index in hostnames.items():
+    try:
+        ip_address = socket.gethostbyname(hostname)
+        hosts[ip_address] = prefix_index
+    except socket.gaierror:
+        pass
 
 def request(flow: http.HTTPFlow) -> None:
     prefix_type = hosts.get(flow.request.host)
