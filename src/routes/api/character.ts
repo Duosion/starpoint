0865c2f4972cfec674d94164180fa448aeb8857f
Duosion <duosiont@gmail.com>
2024-06-21 21:49:34 -0500

    Finish over_limit route.

--- src/routes/api/character.ts
+++ src/routes/api/character.ts
@@ -1,7 +1,7 @@
 // Handles the insertion of mana into characters.
 
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { getAccountPlayers, getPlayerCharacterSync, getPlayerSync, getSession, updatePlayerCharacterSync } from "../../data/wdfpData";
+import { getAccountPlayers, getPlayerCharacterSync, getPlayerItemSync, getPlayerSync, getSession, updatePlayerCharacterSync, updatePlayerItemSync } from "../../data/wdfpData";
 import { generateDataHeaders } from "../../utils";
 import { getCharacterDataSync } from "../../lib/assets";
 import { clientSerializeDate } from "../../data/utils";
@@ -11,6 +11,7 @@ interface OverLimitBody {
     character_id: number
     api_count: number
     use_stack: boolean
+    item_id: number,
     over_limit_count: number
 }
 
@@ -63,33 +64,69 @@ const routes = async (fastify: FastifyInstance) => {
             "message": "No character asset data found."
         })
 
-        // ensure we're in stack mode
-        // TODO: implement non-stack mode uncapping
-        if (!body.use_stack) return reply.status(501).send({
-            "error": "Not Implemented",
-            "message": "Only duplicate uncapping is implemented."
-        })
-
         // calculate new over limit
         const overLimitCount = body.over_limit_count
         const newOverLimit = playerCharacterData.overLimitStep + overLimitCount
-        if (newOverLimit > characterMaxOverLimits[characterAssetData.rarity]) return reply.status(400).send({
+        const characterRarity = characterAssetData.rarity
+        if (newOverLimit > characterMaxOverLimits[characterRarity]) return reply.status(400).send({
             "error": "Bad Request",
             "message": "Character cannot be uncapped further."
         })
 
-        // ensure that the character has enough stack
-        const newStack = playerCharacterData.stack - overLimitCount
-        if (0 > newStack) return reply.status(400).send({
-            "error": "Bad Request",
-            "message": "hCharacter does not have enough duplicates to uncap."
-        })
+        let stack = playerCharacterData.stack
+        const item_list: Record<number, number> = {}
 
-        // update the character
-        updatePlayerCharacterSync(playerId, characterId, {
-            overLimitStep: newOverLimit,
-            stack: newStack
-        })
+        if (body.use_stack) {
+            // stack uncapping
+            
+            // ensure that the character has enough stack
+            stack = stack - overLimitCount
+            if (0 > stack) return reply.status(400).send({
+                "error": "Bad Request",
+                "message": "Character does not have enough duplicates to uncap."
+            })
+
+            // update the character
+            updatePlayerCharacterSync(playerId, characterId, {
+                overLimitStep: newOverLimit,
+                stack: stack
+            })
+        } else {
+            // item uncapping
+            const itemId = body.item_id
+
+            // ensure that the item trying to be used is valid
+            // 5* characters can only be uncapped by item 10003 (awaking_crystal_5)
+            // 4* characters and below can only be uncapped by items 10002 (awaking_crystal_4) and 10001 (awaking_crystal_3)
+            if ( (characterRarity === 5 && itemId !== 10003) 
+                || ( 4 >= characterRarity && (itemId !== 10002 && itemId !== 10001)) 
+            ) return reply.status(400).send({
+                "error": "Bad Request",
+                "message": "Attempted to use invalid item."
+            })
+
+            const itemData = getPlayerItemSync(playerId, itemId)
+            if (itemData === null) return reply.status(400).send({
+                "error": "Bad Request",
+                "message": "Attempted to use unowned item."
+            })
+
+            // make sure that the player has enough of the item
+            const newAmount = itemData - overLimitCount
+            if (0 > newAmount) return reply.status(400).send({
+                "error": "Bad Request",
+                "message": "Not enough of item to uncap."
+            })
+
+            // update the item count
+            updatePlayerItemSync(playerId, itemId, newAmount)
+            item_list[itemId] = newAmount // add to items table
+
+            // update the character
+            updatePlayerCharacterSync(playerId, characterId, {
+                overLimitStep: newOverLimit
+            })
+        }
 
         reply.header("content-type", "application/x-msgpack")
         return reply.status(200).send({
@@ -101,12 +138,13 @@ const routes = async (fastify: FastifyInstance) => {
                     {
                         "over_limit_step": newOverLimit,
                         "character_id": characterId,
-                        "stack": newStack,
+                        "stack": stack,
                         "create_time": clientSerializeDate(playerCharacterData.joinTime),
                         "update_time": clientSerializeDate(new Date()),
                         "join_time": clientSerializeDate(playerCharacterData.joinTime)
                     }
                 ],
+                "item_list": item_list,
                 "mail_arrived": false
             }
         })
