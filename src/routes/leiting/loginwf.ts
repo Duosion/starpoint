53c1f48a805db534f1a06b972c46b278248ec6c3
Duosion <duosiont@gmail.com>
2024-08-15 18:16:50 -0500

    Change sms login return data.

--- src/routes/leiting/loginwf.ts
+++ src/routes/leiting/loginwf.ts
@@ -5,43 +5,35 @@ import { getServerTime } from "../../utils";
 
 const routes = async (fastify: FastifyInstance) => {
 
-    fastify.post("/sdk/v3-3/code_login.do", (_, reply: FastifyReply) => {
-        reply.status(200).send({
-            "status": 0,
-            "data": {
-                "showProtocol": "0",
-                "username": "StarpointUser",
-                "userpwd": "hello",
-                "sid": '1',
-                "bind": "1",
-                "cookie": "1",
-                "phoneBind": "1",
-                "emailBind": "1",
-                "questionBind": "1",
-                "pwd": "hello",
-                "imei": "hello",
-                "serial": "",
-                "phoneNum": "11111111111",
-                "phoneModel": "hello",
-                "phoneActiveKey": "1",
-                "token": "hello",
-                "needActivate": "0",
-                "adult": "1",
-                "age": 20,
-                "realNameAuth": "1",
-                "lang": "cn",
-                "timestamp": getServerTime(),
-                "status": "0",
-                "mmid": "hello",
-                "isFast": "0",
-                "privacyEndDate": "2099-12-31",
-                "registTime": getServerTime(),
-                "guestUpgrade": "0",
-                "noticeId": ""
-            }
-            
-        })
+  fastify.post("/sdk/v3-3/code_login.do", (_, reply: FastifyReply) => {
+    reply.status(200).send({
+      "status": "0",
+      "message": "",
+      "data": {
+        "token": "user_authentication_token",
+        "userId": "1234567890",
+        "username": "testuser",
+        "bind": "1,2",
+        "isFast": "0",
+        "adult": "1",
+        "age": 25,
+        "timestamp": "1678888888",
+        "registTime": "1678888888",
+        "isSetPwd": 1,
+        "code": null,
+        "mmid": "test_mmid",
+        "showProtocol": "1"
+      }
     })
+  })
+
+  fastify.post("/sdk/v3-3/pwd_login.do", (_, reply: FastifyReply) => {
+    const timestamp = getServerTime()
+    reply.status(200).send({
+      "status": "success",
+      "message": {"sid":"user_session_id","token":"auth_token","timestamp":"1678888888","auth":"0","age":25,"showProtocol":"1"}
+    })
+  })
 
 }
 
